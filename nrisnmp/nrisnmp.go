8dbcc1d934a7afdf36be394047f212f8e48d533b
Vlafimir Ulogov <vladimir.ulogov@me.com>
2021-01-24 21:59:04 -0800

    Set and get DB name

--- nrisnmp/nrisnmp.go
+++ nrisnmp/nrisnmp.go
@@ -1,9 +1,29 @@
 package nrisnmp
 
 import (
+  "os"
   "fmt"
 )
 
+var snmpdb string
+
 func Init() {
-  fmt.Println("snmpdb")
+  if os.Getenv("NRISNMPDB") == nil {
+    snmpdb = "/usr/share/nrisnmpdb/nrisnmp.db"
+  } else {
+    snmpdb = os.Getenv("NRISNMPDB")
+  }
+}
+
+func DBName() string {
+  return snmpdb
+}
+
+func SetDBName(n string) bool {
+  info, err := os.Stat(n)
+  if os.IsNotExist(err) {
+      return false
+  }
+  snmpdb = n
+  return true
 }
